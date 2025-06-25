e5bef985f3d61ffa663ea575f4a0acb93d15df00
SheikhIshere <thesheikh255@gmail.com>
2025-06-26 02:01:00 +0600

    changed admin address

--- networking/urls.py
+++ networking/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('back-door-dashbord/', admin.site.urls),
     path('', include('netmath.urls')),
 ]
